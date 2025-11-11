c681520efa3d74e7e5344181d84f6ab202cfde11
Riek Gatluak Geka Reat <riekgatluak25@gmail.com>
2025-11-10 23:25:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -48,12 +48,6 @@ window.addEventListener('load', fadeInOnScroll);
 // Check on scroll
 window.addEventListener('scroll', fadeInOnScroll);
 
-// Form submission handling
-const contactForm = document.querySelector('.contact-form form');
-if (contactForm) {
-    contactForm.addEventListener('submit', function(e) {
-        e.preventDefault();
-        alert('Thank you for your message! We will get back to you soon.');
-        contactForm.reset();
+
     });
 }
